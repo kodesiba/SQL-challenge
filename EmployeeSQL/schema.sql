77e63388424607d043b94202c51d84534649548b
Nathan Kosiba <kosiba384@gmail.com>
2020-01-28 21:10:57 -0500

    update schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,48 +1,48 @@
 -- Schema for setup of EmployeeSQL database
 CREATE TABLE departments (
-  dept_no VARCHAR(10) PRIMARY KEY,
-  dept_name VARCHAR(30)
+  dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+  dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employees (
-  emp_no INT PRIMARY KEY,
-  birth_date DATE,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  emp_no INT PRIMARY KEY NOT NULL,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   gender VARCHAR(3),
-  hire_date DATE
+  hire_date DATE NOT NULL
 );
 
 CREATE TABLE dept_emp (
-  emp_no INT,
-  dept_no VARCHAR(10),
-  from_date DATE,
-  to_date DATE,
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(10) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_manager (
-  dept_no VARCHAR(10),
-  emp_no INT,
-  from_date DATE,
-  to_date DATE,
+  dept_no VARCHAR(10) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INT PRIMARY KEY,
-  salary BIGINT,
-  from_date DATE,
-  to_date DATE,
+  emp_no INT NOT NULL,
+  salary BIGINT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles (
-  emp_no INT,
-  title VARCHAR(50),
-  from_date DATE,
-  to_date DATE,
+  emp_no INT NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );

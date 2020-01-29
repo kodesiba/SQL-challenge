43c882bbfc7ac3e051020387304b79f64df216a9
Nathan Kosiba <kosiba384@gmail.com>
2020-01-28 21:04:27 -0500

    update diagram and schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -4,20 +4,6 @@ CREATE TABLE departments (
   dept_name VARCHAR(30)
 );
 
-CREATE TABLE dept_emp (
-  emp_no INT,
-  dept_no VARCHAR(10),
-  from_date DATE,
-  to_date DATE
-);
-
-CREATE TABLE dept_manager (
-  dept_no VARCHAR(10),
-  emp_no INT PRIMARY KEY,
-  from_date DATE,
-  to_date DATE
-);
-
 CREATE TABLE employees (
   emp_no INT PRIMARY KEY,
   birth_date DATE,
@@ -27,16 +13,36 @@ CREATE TABLE employees (
   hire_date DATE
 );
 
+CREATE TABLE dept_emp (
+  emp_no INT,
+  dept_no VARCHAR(10),
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(10),
+  emp_no INT,
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
 CREATE TABLE salaries (
   emp_no INT PRIMARY KEY,
   salary BIGINT,
   from_date DATE,
-  to_date DATE
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles (
   emp_no INT,
   title VARCHAR(50),
   from_date DATE,
-  to_date DATE
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
